3defc11009b09773e004e3b246fdf9797deaec38
jaymanale <jaymanale29@gmail.com>
2022-07-27 18:38:33 +0530

    Comment add and Minor Improvement

--- src/main/java/com/springboot/app/fekuAPI/controller/UserController.java
+++ src/main/java/com/springboot/app/fekuAPI/controller/UserController.java
@@ -18,30 +18,39 @@ public class UserController {
         this.userService = userService;
     }
 
+    // Save User
+    // POST : http://localhost:8080/api/v1/users
     @PostMapping()
     public ResponseEntity<User> saveUsers(@RequestBody User user) {
         return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
     }
 
+    // Get all USers
+    // GET : http://localhost:8080/api/v1/users
     @GetMapping()
     public ResponseEntity<List<User>> getUsers() {
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
     }
 
+    // Get User by ID
+    // GET : http://localhost:8080/api/v1/users/1
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
         return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
     }
 
+    // Update User by ID
+    // PUT : http://localhost:8080/api/v1/users/1
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User user) {
         return new ResponseEntity<>(userService.updateUser(id, user), HttpStatus.OK);
     }
 
+    // Delete User by ID
+    // DELETE : http://localhost:8080/api/v1/users/1
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id){
-
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
         userService.deleteUser(id);
-        return new ResponseEntity<>(String.format("Employee with ID : %s deleted successfully.",id), HttpStatus.OK);
+        return new ResponseEntity<>(String.format("Employee with ID : %s deleted successfully.", id), HttpStatus.OK);
     }
 }
