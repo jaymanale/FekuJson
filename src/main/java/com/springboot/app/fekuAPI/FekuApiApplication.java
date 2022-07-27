212f8dacfcb41fa52e4fdf8d395823a047687e35
jaymanale <jaymanale29@gmail.com>
2022-07-27 12:50:49 +0530

    Initial Commit

---
+++ src/main/java/com/springboot/app/fekuAPI/FekuApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.fekuAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FekuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FekuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/app/fekuAPI/FekuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.fekuAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FekuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
